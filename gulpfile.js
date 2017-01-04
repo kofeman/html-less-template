d73753e1d4457d987f337b9a62b53ce2f846afc0
Nikolay Starikov <nickolay-starikov@ya.ru>
2017-01-04 17:11:00 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,107 @@
+var gulp = require('gulp'),
+	less = require('gulp-less'),
+	imagemin = require('gulp-imagemin'),
+	imageminPngquant = require('imagemin-pngquant'),
+	concat = require('gulp-concat'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sprite = require("gulp.spritesmith"),
+	fileinclude = require('gulp-file-include');
+
+// Собираем html
+
+gulp.task('html', function() {
+	gulp.src(['src/*.html'])
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: '@file',
+			indent: true
+		}))
+		.pipe(gulp.dest('/'))
+		.on('error', console.log);
+});
+
+//less
+
+gulp.task('less', function() {
+	gulp.src(['src/less/style.less'])
+		.pipe(less())
+		.on('error', console.log)
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('css'));
+});
+
+
+// Собираем JS
+
+//gulp.task('js', function() {
+//	gulp.src(['app/blocks/**/*.js'])
+//		.pipe(concat('main.js'))
+//		.pipe(gulp.dest('./dist/js'));
+//
+//});
+
+// Копируем и минимизируем изображения
+
+gulp.task('images', function() {
+
+	// jpg
+
+	gulp.src(['src/images/**/*.jpg'])
+		.pipe(imagemin())
+		.pipe(gulp.dest('images'));
+
+	// png
+
+	gulp.src(['src/images/**/*.png'])
+		.pipe(imageminPngquant({quality: '65-80', speed: 4})())
+		.pipe(gulp.dest('images'));
+
+
+	// sprites
+
+	gulp.src('images/sprite/**/*') // путь, откуда берем картинки для спрайта
+		.pipe(sprite({
+			imgName: 'images/sprite.png',
+			cssName: '_sprite.css',
+			padding: 20,
+			algorithm: 'top-down',
+			cssOpts: {
+				cssSelector: function (sprite) {
+					return '.' + sprite.name;
+				}
+			}
+		}));
+
+	spriteData.img.pipe(gulp.dest('images')); // путь, куда сохраняем картинку
+	spriteData.css
+		.pipe(gulp.dest('src/less')); // путь, куда сохраняем стили
+
+	gulp.src(['src/less/_sprite.css'])
+		.pipe(rename('_sprite.less'))
+		.pipe(gulp.dest('src/less'));
+
+
+});
+
+
+// gulp watch
+
+gulp.task('watch', function() {
+
+	gulp.watch('src/less/**/*.less', function() {
+		gulp.run('less');
+	});
+	gulp.watch('src/**/*.html', function() {
+		gulp.run('html');
+	});
+	gulp.watch('app/images/*/**', function() {
+		gulp.run('images');
+	});
+});
+
+
+//default task
+
+gulp.task('default', [ 'html', 'less', 'images', 'watch'], function() {});
+
+
---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+$(document).on('ready', function(){
+
+
+
+
+	// remodal fix bug
+
+	//$(document).on('opened', '.remodal', function () {
+	//
+	//
+	//	if ( isWindows() ) {
+	//		$('body').addClass('modal-opened');
+	//		console.log('windows')
+	//	} else {
+	//		console.log('not windows')
+	//	}
+	//});
+	//
+	//$(document).on('closing', '.remodal', function () {
+	//
+	//	$('body').removeClass('modal-opened');
+	//});
+	//
+	//
+	//function isWindows() {
+	//	return navigator.platform.indexOf('Win') > -1
+	//}
+});
